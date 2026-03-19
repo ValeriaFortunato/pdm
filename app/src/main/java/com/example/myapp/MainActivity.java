796d14f4dfa547838ccea930b62694bb10368a18
ValeriaFortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-03-19 11:41:40 -0300

    o q eu consegui de codigo

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,9 +1,8 @@
 package com.example.myapp;
 
-
-
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +12,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     Button b;
+    EditText edMin, edMax;
     int contador = 0;
 
     @Override
@@ -22,9 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
         b = findViewById(R.id.button);
         TextView tv = findViewById(R.id.tv);
+        edMin=findViewById(R.id.edMin);
+        edMax=findViewById(R.id.edMax);
 
         b.setOnClickListener(  v -> {
-            contador++;
+            int min = Integer.parseInt(edMin.getText().toString());
+            int max = Integer.parseInt(edMax.getText().toString());
             Random random = new Random();
             int r = random.nextInt(100);
             tv.setText(Integer.toString(r));
