6a0ba6451a1f088df38f5afb0744f8f0b368fe4f
Romulo Beninca <rbeninca@gmail.com>
2026-04-15 11:17:52 -0300

    ghjk

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -8,21 +8,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button bntVoltar, bntAvancar;
-    ImageView imageView;
-    Integer imagens[] = new Integer[]{
-            R.drawable.cachorro,
-            R.drawable.gardem,
-            R.drawable.happy,
-            R.drawable.patinho,
-            R.drawable.porquinho
-    };
-
-    int posicao = 0;
+    EditText Altura, Peso;
+    TextView tvIMC;
+    Button bntcalc;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +23,25 @@ public class MainActivity extends AppCompatActivity {
         //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        bntVoltar = findViewById(R.id.buttonVoltar);
-        bntAvancar = findViewById(R.id.buttonAvancar);
-        imageView = findViewById(R.id.imageView);
+        Peso=findViewById(R.id.Peso);
+        Altura=findViewById(R.id.Altura);
+        tvIMC=findViewById(R.id.tvIMC);
+        img=findViewById(R.id.img);
+        bntcalc = findViewById(R.id.Calcular);
 
-        bntAvancar.setOnClickListener(v -> {
-            imageView.setImageResource(imagens[posicao]);
+
+        bntcalc.setOnClickListener(v -> {
+            String strAltura = Altura.getText().toString();
+            Double alt = Double.parseDouble(strAltura);
+
+            String strPeso = Peso.getText().toString();
+            Double peso = Double.parseDouble(strPeso);
+
+            Double conta = peso/(alt * alt);
+            DecimalFormat dc = new DecimalFormat("##.##");
+
+            tvIMC.setText(dc.format(conta));
+
         });
-
     }
-}}
+}
