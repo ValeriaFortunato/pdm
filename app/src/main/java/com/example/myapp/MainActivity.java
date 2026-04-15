59603b7caad722dcea239614aa070d7ae5bcdc04
ValeriaFortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-04-15 10:49:19 -0300

    arrumando

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.app;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -10,27 +10,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.io.File;
-
 public class MainActivity extends AppCompatActivity {
 
     Button b;
-    int contador = 0;
+    int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        b = findViewById(R.id.button);
-        TextView tv = findViewById(R.id.tv);
-
+        TextView tv = findViewById(R.id.TextView);
+        b= findViewById(R.id.button);
         b.setOnClickListener(v -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
-            b.setText("Plin");
+            cont++;
+            tv.setText(Integer.toString(cont));
         });
 
     }
-
 }
