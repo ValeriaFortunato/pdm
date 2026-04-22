8ab6bf5e9a2cdf39c72c0329a1cd0001d0adee76
Valeria Fortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-04-22 11:23:23 -0300

    tela nova tela

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,47 +1,36 @@
 package com.example.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText Altura, Peso;
-    TextView tvIMC;
-    Button bntcalc;
-    ImageView img;
+    EditText et;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //EdgeToEdge.enable(this);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        Peso=findViewById(R.id.Peso);
-        Altura=findViewById(R.id.Altura);
-        tvIMC=findViewById(R.id.tvIMC);
-        img=findViewById(R.id.img);
-        bntcalc = findViewById(R.id.Calcular);
+        et = findViewById(R.id.editTextText);
+        btn = findViewById(R.id.button);
 
+        btn.setOnClickListener(v -> {
+            String msg = et.getText().toString();
 
-        bntcalc.setOnClickListener(v -> {
-            String strAltura = Altura.getText().toString();
-            Double alt = Double.parseDouble(strAltura);
+            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            intent.putExtra("msg", msg);
 
-            String strPeso = Peso.getText().toString();
-            Double peso = Double.parseDouble(strPeso);
-
-            Double conta = peso/(alt * alt);
-            DecimalFormat dc = new DecimalFormat("##.##");
-
-            tvIMC.setText(dc.format(conta));
-
+            startActivity(intent);
         });
+
+
     }
 }
---
+++ s
@@ -0,0 +1,12 @@
+  master[m
+* [32mnovatela[m
+  [31mremotes/origin/HEAD[m -> origin/master
+  [31mremotes/origin/IMC[m
+  [31mremotes/origin/contaclick[m
+  [31mremotes/origin/i18[m
+  [31mremotes/origin/i18n[m
+  [31mremotes/origin/master[m
+  [31mremotes/origin/novatela[m
+  [31mremotes/origin/slideshow[m
+  [31mremotes/origin/sorteio[m
+  [31mremotes/origin/sorteiointervalo[m
