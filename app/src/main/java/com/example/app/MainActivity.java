55e28b39a16ae12c022bf99eb369e2f2724f3461
aluno <valeria.f2007@aluno.ifsc.edu.br>
2026-04-23 09:08:14 -0300

    arrumando de novoooo

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
+        getString(R.string.app_name);
     }
 }
--- app/src/test/java/com/example/app/ExampleUnitTest.java
+++ app/src/test/java/com/example/app/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapp;
+package com.example.app;
 
 import org.junit.Test;
 
