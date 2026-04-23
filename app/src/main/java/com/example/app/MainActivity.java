1aca6b481bfe6b04b2eae6dff97743b8dcf814b3
aluno <valeria.f2007@aluno.ifsc.edu.br>
2026-04-23 10:33:00 -0300

    novatela arrumada

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.app;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -20,8 +19,8 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        et = findViewById(R.id.editText);
-        btn = findViewById(R.id.btnFechar);
+        et = findViewById(R.id.EditText);
+        btn = findViewById(R.id.button);
 
         btn.setOnClickListener(v -> {
             String msg = et.getText().toString();
@@ -33,3 +32,4 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
--- app/src/main/java/com/example/app/MainActivity2.java
+++ app/src/main/java/com/example/app/MainActivity2.java
@@ -10,7 +10,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.example.myapp.R;
 
 public class MainActivity2 extends AppCompatActivity {
     Button bntFechar;
