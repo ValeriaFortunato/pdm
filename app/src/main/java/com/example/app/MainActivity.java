cf0f818ef1af16f2a05b8565884b5539cd68ee77
aluno <valeria.f2007@aluno.ifsc.edu.br>
2026-04-23 09:29:45 -0300

    sorteio

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.app;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    Button b;
+    int contador=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getString(R.string.app_name);
+        b=findViewById(R.id.button);
+        TextView tv = findViewById(R.id.tv);
+        b.setOnClickListener(v -> {
+            contador++;
+            tv.setText(Integer.toString(contador));
+            Random random=new Random();
+            int r= random.nextInt(bound: 100);
+        });
+    }
+}
