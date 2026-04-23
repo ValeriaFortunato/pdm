cbdc80886062f78887f935347130388f956b4753
aluno <valeria.f2007@aluno.ifsc.edu.br>
2026-04-23 09:52:47 -0300

    sorteiointervalo

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,24 +1,64 @@
 package com.example.app;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    TextView tv;
+    EditText edmin, edmax;
+
+    int contador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        b = findViewById(R.id.button);
+        tv = findViewById(R.id.tv);
+
+        edmin = findViewById(R.id.edMin);
+        edmax = findViewById(R.id.edMax);
+
+        b.setOnClickListener(v -> {
+
+            String minStr = edmin.getText().toString();
+            String maxStr = edmax.getText().toString();
+
+            if (minStr.isEmpty()) {
+                edmin.setError("Informe o valor mínimo");
+                return;
+            }
+
+            if (maxStr.isEmpty()) {
+                edmax.setError("Informe o valor máximo");
+                return;
+            }
+
+            int min = Integer.parseInt(minStr);
+            int max = Integer.parseInt(maxStr);
+
+            if (min >= max) {
+                edmax.setError("O máximo deve ser maior que o mínimo");
+                return;
+            }
+
+            contador++;
+
+            Random random = new Random();
+            int r = random.nextInt(max - min) + min;
+
+            tv.setText(String.valueOf(r));
         });
     }
 }
