212882bfad5185c7c4090b5ffeef5e60fb7e2aad
valeria <valeria.f2007@aluno.ifsc.edu>
2026-05-20 11:43:05 -0300

    commit sqlite

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapp;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,14 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    SQLiteDatabase sqLiteDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.app_name);
+         sqLiteDatabase =openOrCreateDatabase("notas" , MODE_PRIVATE, null);
+         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT , nota TEXT)");
+         //String titulo = "João";
+         //sqLiteDatabase.execSQL("INSERT INTO notas VALUES(1,'"+titulo+"', '')");
 
+        /* nomeColuna, valor */
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo" , "joão");
+        contentValues.put("nota", "777");
+
+        sqLiteDatabase.insert("notas" , null, contentValues);
+
+        //Recuperar Dados do SQlite
+        Cursor c=sqLiteDatabase.rawQuery("SELECT id*10,titulo , nota  FROM notas" , null);
 
     }
 }
