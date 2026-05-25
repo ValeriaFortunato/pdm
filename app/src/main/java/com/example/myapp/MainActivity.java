8ead0692314ff8f299af001cd3c9e203a33a799f
ValeriaFortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-05-25 20:50:39 -0300

    arrumando novatela

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.myapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -28,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         btn = findViewById(R.id.button);
-        et = findViewById(R.id.editText);
+        et = findViewById(R.id.et);
 
 
         btn.setOnClickListener(v -> {
