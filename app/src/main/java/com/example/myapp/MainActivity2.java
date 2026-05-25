a3b87f9840494f87790ed53b79bb02d57be156f7
ValeriaFortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-05-25 20:50:03 -0300

    arrumando novatela

--- app/src/main/java/com/example/myapp/MainActivity2.java
+++ app/src/main/java/com/example/myapp/MainActivity2.java
@@ -1,11 +1,9 @@
 package com.example.myapp;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,28 +11,42 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
 public class MainActivity2 extends AppCompatActivity {
+
     Button botaoFechar;
     TextView tv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
+            v.setPadding(
+                    systemBars.left,
+                    systemBars.top,
+                    systemBars.right,
+                    systemBars.bottom
+            );
+
             return insets;
         });
 
+        botaoFechar = findViewById(R.id.btnfechar);
+        tv = findViewById(R.id.tv2);
 
-        botaoFechar=findViewById(R.id.btnfechar);
-        tv=findViewById(R.id.tv2);
         Intent i = getIntent();
-        Bundle b=i.getExtras();
+        Bundle b = i.getExtras();
+
         String msg = b.getString("msg");
+
         tv.setText(msg);
+
         botaoFechar.setOnClickListener(v -> {
             finish();
         });
+    }
+}
