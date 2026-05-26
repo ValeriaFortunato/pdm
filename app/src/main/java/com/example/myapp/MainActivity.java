9a4fc629719b0f3aeef0b062f3f34706777bcca1
ValeriaFortunato <valeria.f2007@aluno.ifsc.edu.br>
2026-05-25 21:03:42 -0300

    arrumando sorteiointervalo

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -1,11 +1,10 @@
-package com.example.app;
+package com.example.myapp;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
@@ -16,17 +15,13 @@ public class MainActivity extends AppCompatActivity {
     TextView tv;
     EditText edmin, edmax;
 
-    int contador = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         b = findViewById(R.id.button);
         tv = findViewById(R.id.tv);
-
         edmin = findViewById(R.id.edMin);
         edmax = findViewById(R.id.edMax);
 
@@ -36,27 +31,21 @@ public class MainActivity extends AppCompatActivity {
             String maxStr = edmax.getText().toString();
 
             if (minStr.isEmpty()) {
-                edmin.setError("Informe o valor mínimo");
+                edmin.setError("Informe um valor mínimo");
                 return;
             }
 
             if (maxStr.isEmpty()) {
-                edmax.setError("Informe o valor máximo");
+                edmax.setError("Informe um valor máximo");
                 return;
             }
 
             int min = Integer.parseInt(minStr);
             int max = Integer.parseInt(maxStr);
 
-            if (min >= max) {
-                edmax.setError("O máximo deve ser maior que o mínimo");
-                return;
-            }
-
-            contador++;
-
             Random random = new Random();
-            int r = random.nextInt(max - min) + min;
+
+            int r = random.nextInt((max - min) + 1) + min;
 
             tv.setText(String.valueOf(r));
         });
